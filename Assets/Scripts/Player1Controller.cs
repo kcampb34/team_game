250fb54e22e29f1ca1daf82a024d6950952943c2
kcampb34 <kcampb34@student.fitchburgstate.edu>
2024-11-05 18:02:22 -0500

    Code Added

---
+++ Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controller : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10.0f;
+    private float turnSpeed = 50;
+    public float xRange = 700;
+    public float zRange = 700;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Keeping the player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
+        transform.Translate(Vector3.right * -verticalInput * Time.deltaTime * speed);
+
+    }
+}
